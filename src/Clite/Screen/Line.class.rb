fc6a5d9c7ee2a005418e3866abc57fc285fd711c
Anatoly Chernow <chertoly@gmail.com>
2017-11-11 02:48:24 +0200

    Change Line#cursor for all the events

--- src/Clite/Screen/Line.class.rb
+++ src/Clite/Screen/Line.class.rb
@@ -1,27 +1,29 @@
 def initialize
   @string = String.new
+  @cursor = 0
 end
 
 def << event
   case event
   when String
     @string << event
+    @cursor += event.size
   when Symbol
     case event
     when :backspace
       @string = @string[0..-2]
+      @cursor -= 1 unless @cursor.zero?
     end
   when Array
     case event
     when [:ctrl, ?u]
       @string.clear
+      @cursor = 0
     end
   end
 end
 
-def cursor
-  @string.size
-end
+attr_reader :cursor
 
 def to_s
   @string
