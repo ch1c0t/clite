b2de4487911ca03b033b91f0ef0ce32927c50d5d
Anatoly Chernow <chertoly@gmail.com>
2017-11-15 23:29:04 +0200

    Factor out Keyboard::SEQUENCES

--- src/Clite/Keyboard:Enumerator.class.rb
+++ src/Clite/Keyboard:Enumerator.class.rb
@@ -1,29 +1,21 @@
+SEQUENCES = {
+  "\r" => :enter,
+  "\t" => :tab,
+  "\u0003" => [:ctrl, ?c],
+  "\u0015" => [:ctrl, ?u],
+  "\u0001" => [:ctrl, ?a],
+  "\u0005" => [:ctrl, ?e],
+  "\u0011" => [:ctrl, ?q],
+  "\v"     => [:ctrl, ?k],
+  "\u007F" => :backspace,
+}
 def initialize
   super do |y|
     loop do
-      key = case (key = STDIN.getch)
-            when "\r"
-              :enter
-            when "\t"
-              :tab
-            when "\u0003"
-              [:ctrl, ?c]
-            when "\u0015"
-              [:ctrl, ?u]
-            when "\u0001"
-              [:ctrl, ?a]
-            when "\u0005"
-              [:ctrl, ?e]
-            when "\u0011"
-              [:ctrl, ?q]
-            when "\v"
-              [:ctrl, ?k]
-            when "\u007F"
-              :backspace
-            else
-              key
-            end
-      y << key
+      key = STDIN.getch
+      value = SEQUENCES[key] || key
+
+      y << value
     end
   end
 end
